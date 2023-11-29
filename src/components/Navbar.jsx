9761efc85b0031506dbd902afea0d01aa07edfcf
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2023-11-29 10:01:58 +0200

    Went back and added the correct episodes files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,16 @@ import { NavLink } from "react-router-dom";
 
 export default function Navbar({ children }) {
   return (
-    <nav className="flex items-center justify-evenly  py-8 bg-amber-500">
-      <ul className="flex  justify-around">
+    <nav className="flex items-center justify-center py-8 bg-amber-500">
+      <ul className="flex gap-9 justify-between ">
+        <li>PlayList</li>
+
+        <li>Preview</li>
+
         <li>{children}</li>
         <li>
           <button>Favourites</button>
         </li>
-        <li></li>
-        <li></li>
-        <li></li>
       </ul>
     </nav>
   );
--- src/components/Shows.jsx
+++ src/components/Shows.jsx
@@ -14,8 +14,7 @@ export default function Shows({
   const [selectedSeason, setSelectedSeason] = React.useState(null);
 
   function getSeasonClicked(season) {
-    setSelectedSeason(season);
-    setOpenEpisodes((prevState) => !prevState);
+    setSelectedSeason((prevSeason) => (prevSeason === season ? null : season));
   }
   const { title, updated, seasons, id, description, image, genres } = show;
 
@@ -31,7 +30,7 @@ export default function Shows({
         }
 
         const data = await res.json();
-        console.log(data);
+
         setShow(data).catch((error) => {
           console.error("Error during fetch:", error);
         });
@@ -44,8 +43,8 @@ export default function Shows({
   return (
     <>
       {show ? (
-        <section className="flex-col bg-slate-400">
-          <div className="flex justify-between">
+        <section className="flex-col">
+          <div className="flex bg-slate-400 justify-between">
             <div>
               <img src={image} />
             </div>
@@ -61,14 +60,17 @@ export default function Shows({
             {seasons
               ? seasons.map((eachSeason) => {
                   return (
-                    <ShowSeason
-                      updated={updated}
-                      item={eachSeason}
-                      key={seasons.indexOf(eachSeason)}
-                      onDateConversion={onDateConversion}
-                      numbering={seasons.indexOf(eachSeason)}
-                      getSeasonClicked={getSeasonClicked}
-                    />
+                    <>
+                      <ShowSeason
+                        updated={updated}
+                        item={eachSeason}
+                        key={seasons.indexOf(eachSeason)}
+                        onDateConversion={onDateConversion}
+                        numbering={seasons.indexOf(eachSeason)}
+                        getSeasonClicked={getSeasonClicked}
+                        selectedSeason={selectedSeason}
+                      />
+                    </>
                   );
                 })
               : ""}
@@ -87,28 +89,51 @@ function ShowSeason({
   onDateConversion,
   numbering,
   getSeasonClicked,
+  selectedSeason,
 }) {
   return (
     <div
-      className="m-1 flex bg-slate-100 rounded-b-lg"
-      onClick={() => getSeasonClicked(item.episodes)}
+      className="m-1 flex-col  rounded-b-lg"
+      onClick={() => getSeasonClicked(item)}
     >
-      <div className="text-2xl pt-10 font-bold">{numbering + 1}</div>
-      <div className=" h-40">
-        <img src={item.image} className="object-contain   h-40 items-center " />
-      </div>
-      <div className="flex-col   mx-10">
-        <div className="ps-10 pt-9">
-          <h3 className="text-xl font-bold">{item.title}</h3>
+      <div className="flex bg-red-100">
+        <div className="flex">
+          <div className="text-2xl  pt-10 font-bold">{numbering + 1}</div>
+          <div className=" h-40">
+            <img
+              src={item.image}
+              className="object-contain   h-40 items-center "
+            />
+          </div>
         </div>
+        <div className="flex-col mx-10">
+          <div className="ps-10 pt-9">
+            <h3 className="text-xl font-bold">{item.title}</h3>
+          </div>
 
-        <div className="grid pt-16    self-end grid-flow-col gap-14">
-          <p className="text-lg font-medium">
-            No Episodes {item.episodes.length}
-          </p>
-          <p className="text-lg font-medium">🗓️ {onDateConversion(updated)}</p>
+          <div className="grid pt-16    self-end grid-flow-col gap-14">
+            <p className="text-lg font-medium">
+              No Episodes {item.episodes.length}
+            </p>
+            <p className="text-lg font-medium">
+              🗓️ {onDateConversion(updated)}
+            </p>
+          </div>
         </div>
       </div>
+      <ul className={`my-2 ${selectedSeason === item ? "block" : "hidden"}`}>
+        {item.episodes.map((episode, episodeIndex) => (
+          <li className="p-6 my-1 rounded-lg bg-slate-300" key={episodeIndex}>
+            <h4 className="text-lg font-bold">{episode.title}</h4>
+            <p>{episode.description}</p>
+            {/* Audio player for the episode */}
+            <audio controls>
+              <source src={episode.file} type="audio/mp3" />
+            </audio>
+            {/* Favorite button */}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
